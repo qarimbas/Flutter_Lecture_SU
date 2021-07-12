95fe14bdf1630cb4c07380bdaf004346e69b1fa8
Baris Kerem Kurt <kkurt@sabanciuniv.edu>
2021-07-12 12:30:37 +0300

    flutter basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Text('Hello World!'));
+void main() => runApp(MaterialApp(
+  home: MyApp(),
+));
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CS310'),
+        centerTitle: true,
+      ),
+      body: Center(
+      child: Text('Hello World!'),
+  ),
+    );
+  }
 }
